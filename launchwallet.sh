dfe2b7f894b5238f6e0aa8fc64b6905595d4e334
GeekBlogTV <jamesk@linux.edu>
2017-06-29 22:31:09 -0500

    init

---
+++ launchwallet.sh
@@ -0,0 +1,2 @@
+./monero-wallet-rpc --disable-rpc-login --trusted-daemon --rpc-bind-port 8082 --wallet-file teracycle.main.wallet
+
---
+++ website_example/config.js
@@ -0,0 +1,17 @@
+var api = "http://teracycle.net:8117";
+
+var coinUnits = 1000000000000;
+
+var poolHost = "teracycle.net";
+
+var irc = "irc.freenode.net/#monero-pools";
+
+var email = "support@cryppit.com";
+
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/xmr/block/";
+
+var transactionExplorer = "http://chainradar.com/xmr/transaction/";
