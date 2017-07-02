e39aae33960bebeadaf42ec056abaf0351694df2
GeekBlogTV <jamesk@linux.edu>
2017-07-02 14:22:30 -0500

    3456

--- setup/install.sh
+++ setup/install.sh
@@ -8,35 +8,18 @@ echo "  \__\___|_|  \__,_|\___|\__, |\___|_|\___|  "
 echo "                         |___/               "
 echo "                                    v0.9.01  "
 
-echo "This install will fail if you are running on anything other than a brand new Ubuntu 16x install."
-sleep 15
-echo "Continuing install, this will prompt you for your password if you're not already running as root and you didn't enable passwordless sudo.  Please do not run me as root!"
 if [[ `whoami` == "root" ]]; then
     echo "You ran me as root! Do not run me as root!"
     exit 1
 fi
 CURUSER=$(whoami)
-echo "[Modifying timezone]"
-sleep 10
-echo "Etc/UTC" | sudo tee -a /etc/timezone
-sudo rm -rf /etc/localtime
-sudo ln -s /usr/share/zoneinfo/Zulu /etc/localtime
-sudo dpkg-reconfigure -f noninteractive tzdata
-sudo systemctl enable ntp
-echo "[tzdata complete]"
-sleep 15
-echo ""
-echo "[Installing dependencies]
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git curl ntp build-essential screen cmake pkg-config libboost-all-dev redis-server libevent-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev libgtest-dev lmdb-utils libzmq3-dev libssl-dev
 cd ~
-echo "[Downloading the latest teracycle-pool]"
-sleep 15
 git clone https://github.com/teracycle/teracycle-pool.git 
-sleep 15
 echo "[compiling and installing monero]"
-cd /usr/local/src
+cd ~
 sudo git clone https://github.com/monero-project/monero.git
 cd monero
 sudo git checkout v0.10.3.1
@@ -45,8 +28,6 @@ sudo make -j$(nproc)
 sudo cp ~/teracycle-pool/deployment/monero.service /lib/systemd/system/
 sudo useradd -m monerodaemon -d /home/monerodaemon
 BLOCKCHAIN_DOWNLOAD_DIR=$(sudo -u monerodaemon mktemp -d)
-sleep 15
-echo "[Syncing Blockchain]"
 sudo -u monerodaemon wget --limit-rate=50m -O $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw https://downloads.getmonero.org/blockchain.raw # not sure if i want this to be rate limited, but ill test it
 sudo -u monerodaemon /usr/local/src/monero/build/release/bin/monero-blockchain-import --input-file $BLOCKCHAIN_DOWNLOAD_DIR/blockchain.raw --batch-size 20000 --database lmdb#fastest --verify off --data-dir /home/monerodaemon/.bitmonero
 sudo -u monerodaemon rm -rf $BLOCKCHAIN_DOWNLOAD_DIR
