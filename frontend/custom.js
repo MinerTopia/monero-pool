767e35235ed7bafdacde1f824f4ed017268d8bda
Yan Nunes <yannunes@gmail.com>
2017-07-04 21:04:19 -0300

    Side bar closes when item is selected (#1)
    * Added global variable to controle side-bar opening and global function for DOM handling of the sidebar
    
    * Close sidebar when navigating is working

--- frontend/custom.js
+++ frontend/custom.js
@@ -1,1 +1,24 @@
 /* Insert your pool's unique Javascript here */
+isOpen = true
+
+function hamburger_cross(isOpen, trigger, overlay) {
+
+  trigger = trigger || $('.hamburger')
+  overlay = overlay || $('.overlay')
+  sidebar = $('#wrapper')
+  isOpen = isOpen === true
+
+  if (isOpen == true) { // If open, close
+    overlay.hide()
+    trigger.addClass('is-closed')
+    trigger.removeClass('is-open')
+    sidebar.removeClass('toggled')
+  } else {              // If close, open
+    overlay.show()
+    trigger.removeClass('is-closed')
+    trigger.addClass('is-open')
+    sidebar.addClass('toggled')
+  }
+
+  return !isOpen
+}
